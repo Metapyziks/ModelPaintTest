d47a483a2415f6d5104e411d0eb4f20f1579ccee
James King <james.king@facepunchstudios.com>
2017-02-23 19:06:41 +0000

    Now done in one pass

---
+++ Assets/ModelPaint.shader
@@ -0,0 +1,74 @@
+﻿// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+
+Shader "Unlit/ModelPaint"
+{
+    Properties
+    {
+        _PaintTexture( "Paint Texture", 2D ) = "white" {}
+        _CursorParams( "Cursor Pos", Vector ) = (0.5, 0.5, 0.125, 4)
+        _Color( "Color", Color ) = (1, 1, 1, 1)
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Transparent" }
+        LOD 100
+
+        Blend SrcAlpha OneMinusSrcAlpha
+        ZWrite Off
+
+        Cull Off
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            
+            #include "UnityCG.cginc"
+
+            sampler2D _PaintTexture;
+            float4 _CursorParams;
+            float4 _Color;
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float3 normal: NORMAL;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float3 screenPos : TEXCOORD1;
+                float4 vertex : SV_POSITION;
+            };
+            
+            v2f vert (appdata v)
+            {
+                v2f o;
+
+                float2 uv = v.uv;
+                o.vertex = float4(uv*float2(1, -1) * 2 - float2(1, -1), 0, 1);
+
+                float3 worldPos = mul( unity_ObjectToWorld, v.vertex ).xyz;
+                float3 worldDir = normalize( UnityWorldSpaceViewDir( worldPos ) );
+                float normalDot = max(0, dot( UnityObjectToWorldNormal( v.normal ), worldDir ));
+
+                float4 clipPos = UnityObjectToClipPos(v.vertex);
+                o.uv = float2(v.uv.x, 1 - v.uv.y);
+                o.screenPos = float3((clipPos.xy / clipPos.w) * 0.5 + float2(0.5, 0.5), normalDot);
+                return o;
+            }
+            
+            fixed4 frag (v2f i) : SV_Target
+            {
+                float dist = length( i.screenPos.xy - _CursorParams.xy ) * _CursorParams.w - _CursorParams.z;
+                float alpha = clamp( (1.0 - dist) * i.screenPos.z, 0.0, 1.0 );
+
+                return float4(_Color.rgb, _Color.a * alpha);
+            }
+            ENDCG
+        }
+    }
+}
--- Assets/PaintTest.cs
+++ Assets/PaintTest.cs
@@ -6,9 +6,7 @@ public class PaintTest : MonoBehaviour
 {
     public Camera Camera;
     public MeshRenderer Model;
-    public RenderTexture UnwrapTarget;
     public RenderTexture PaintTarget;
-    public Material UnwrapMaterial;
     public Material PaintMaterial;
 
     public float BrushEdge = 16f;
@@ -18,14 +16,6 @@ public class PaintTest : MonoBehaviour
 
     void Start()
     {
-        if ( UnwrapTarget == null )
-        {
-            UnwrapTarget = new RenderTexture( 512, 512, 1, RenderTextureFormat.RGFloat );
-
-            GetComponent<MeshRenderer>().material.mainTexture = UnwrapTarget;
-            PaintMaterial.SetTexture( "_UnwrappedMap", UnwrapTarget );
-        }
-
         if ( PaintTarget == null )
         {
             PaintTarget = new RenderTexture( 512, 512, 1, RenderTextureFormat.ARGB32 );
@@ -36,43 +26,31 @@ public class PaintTest : MonoBehaviour
 
     private void Paint()
     {
-        if ( Camera == null || UnwrapMaterial == null || Model == null ) return;
-
-        if ( Camera.current != Camera ) return;
+        if ( Camera == null || PaintMaterial == null || Model == null ) return;
 
         var mesh = Model.GetComponent<MeshFilter>().sharedMesh;
-        var quad = GetComponent<MeshFilter>().sharedMesh;
-
-        Graphics.SetRenderTarget( UnwrapTarget );
-
-        UnwrapMaterial.SetPass( 0 );
-
-        GL.PushMatrix();
-        GL.LoadProjectionMatrix( Camera.projectionMatrix );
-
-        GL.Clear( true, true, Color.black );
-
-        Graphics.DrawMeshNow( mesh, Model.transform.localToWorldMatrix, 0 );
-
-        GL.PopMatrix();
 
         Graphics.SetRenderTarget( PaintTarget );
 
         var cursorPos = Camera.ScreenToViewportPoint( Input.mousePosition );
-        Debug.Log( cursorPos );
-
+        
         PaintMaterial.SetVector( "_CursorParams", new Vector4(cursorPos.x, 1f - cursorPos.y, BrushRadius / Camera.pixelHeight, Camera.pixelHeight / BrushEdge) );
         PaintMaterial.SetColor( "_Color", BrushColor );
         PaintMaterial.SetPass( 0 );
 
-        Graphics.DrawMeshNow( quad, Matrix4x4.identity );
-        
+        GL.PushMatrix();
+        GL.LoadProjectionMatrix( Camera.projectionMatrix );
+
+        GL.Clear( true, false, Color.black );
+        Graphics.DrawMeshNow( mesh, Model.transform.localToWorldMatrix, 0 );
+
+        GL.PopMatrix();
+
         Graphics.SetRenderTarget( null );
     }
 
     void Update()
     {
-        if ( Input.GetMouseButton( 0 ) )
-            Paint();
+         if (Input.GetMouseButton( 0 )) Paint();
     }
 }
