e37ccf52dd37c297acd21108cf8405a6510d0993
James King <james.king@facepunchstudios.com>
2017-02-23 17:45:05 +0000

    Added project

---
+++ Assets/PaintTest.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintTest : MonoBehaviour
+{
+    public Camera Camera;
+    public MeshRenderer Model;
+    public RenderTexture UnwrapTarget;
+    public RenderTexture PaintTarget;
+    public Material UnwrapMaterial;
+    public Material PaintMaterial;
+
+    public float BrushEdge = 16f;
+    public float BrushRadius = 128f;
+
+    public Color BrushColor = Color.white;
+
+    void Start()
+    {
+        if ( UnwrapTarget == null )
+        {
+            UnwrapTarget = new RenderTexture( 512, 512, 1, RenderTextureFormat.RGFloat );
+
+            GetComponent<MeshRenderer>().material.mainTexture = UnwrapTarget;
+            PaintMaterial.SetTexture( "_UnwrappedMap", UnwrapTarget );
+        }
+
+        if ( PaintTarget == null )
+        {
+            PaintTarget = new RenderTexture( 512, 512, 1, RenderTextureFormat.ARGB32 );
+
+            Model.material.mainTexture = PaintTarget;
+        }
+    }
+
+    private void Paint()
+    {
+        if ( Camera == null || UnwrapMaterial == null || Model == null ) return;
+
+        if ( Camera.current != Camera ) return;
+
+        var mesh = Model.GetComponent<MeshFilter>().sharedMesh;
+        var quad = GetComponent<MeshFilter>().sharedMesh;
+
+        Graphics.SetRenderTarget( UnwrapTarget );
+
+        UnwrapMaterial.SetPass( 0 );
+
+        GL.PushMatrix();
+        GL.LoadProjectionMatrix( Camera.projectionMatrix );
+
+        GL.Clear( true, true, Color.black );
+
+        Graphics.DrawMeshNow( mesh, Model.transform.localToWorldMatrix, 0 );
+
+        GL.PopMatrix();
+
+        Graphics.SetRenderTarget( PaintTarget );
+
+        var cursorPos = Camera.ScreenToViewportPoint( Input.mousePosition );
+        Debug.Log( cursorPos );
+
+        PaintMaterial.SetVector( "_CursorParams", new Vector4(cursorPos.x, 1f - cursorPos.y, BrushRadius / Camera.pixelHeight, Camera.pixelHeight / BrushEdge) );
+        PaintMaterial.SetColor( "_Color", BrushColor );
+        PaintMaterial.SetPass( 0 );
+
+        Graphics.DrawMeshNow( quad, Matrix4x4.identity );
+        
+        Graphics.SetRenderTarget( null );
+    }
+
+    void Update()
+    {
+        if ( Input.GetMouseButton( 0 ) )
+            Paint();
+    }
+}
---
+++ Assets/UvUnwrap.shader
@@ -0,0 +1,49 @@
+﻿Shader "Unlit/UvUnwrap"
+{
+    Properties
+    {
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+            
+            v2f vert (appdata v)
+            {
+                v2f o;
+
+                float2 uv = v.uv;
+                o.vertex = float4(uv*float2(1, -1) * 2 - float2(1, -1), 0, 1);
+                float4 clipPos = UnityObjectToClipPos(v.vertex);
+                o.uv = clipPos.xy / clipPos.w;
+                return o;
+            }
+            
+            fixed4 frag (v2f i) : SV_Target
+            {
+                return fixed4(i.uv * 0.5 + float2(0.5, 0.5), 1, 1);
+            }
+            ENDCG
+        }
+    }
+}
